c5c8a860d9435ca91ca9b67440df267d42d686c9
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-17 21:50:08 +0530

    ts first file created

---
+++ 01 Intro/intro.js
@@ -0,0 +1,8 @@
+//first Ts file.
+//creating obj in ts
+var user = {
+    name: "Sahil",
+    age: 25
+};
+console.log(user.name);
+console.log(user.age);
---
+++ 01 Intro/intro.ts
@@ -0,0 +1,11 @@
+//first Ts file.
+
+//creating obj in ts
+const user = {
+    name: "Sahil",
+    age: 25
+}
+
+console.log(user.name);
+console.log(user.age);
+
