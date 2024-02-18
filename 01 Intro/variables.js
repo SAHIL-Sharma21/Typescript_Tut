9f2de0a43aa98810bb11ee5090c7cc84fe78fcda
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-18 22:35:05 +0530

    string datatype

--- 01 Intro/variables.js
+++ 01 Intro/variables.js
@@ -1,5 +1,14 @@
+"use strict";
 //here we are learning how to define variables in ts.
 // Syntax:: let variableName : Type = value 
 //here Type is datatype. this is how we define variables in ts
+Object.defineProperty(exports, "__esModule", { value: true });
+//let have a string as a datatype.
 var myName = "Hello, Sahil";
 console.log(myName);
+var word = myName.charAt(2);
+console.log(word);
+//let have number as a datatrype
+var myage;
+myage = 24;
+console.log(myage);
--- 01 Intro/variables.ts
+++ 01 Intro/variables.ts
@@ -2,7 +2,21 @@
 // Syntax:: let variableName : Type = value 
 //here Type is datatype. this is how we define variables in ts
 
+
+//let have a string as a datatype.
 let myName: string = "Hello, Sahil";
 console.log(myName);
 
-export default {}; //temporary solution for removing the error.
+const word: string =  myName.charAt(2);
+console.log(word);
+
+
+//let have number as a datatrype
+let myage: number;
+myage = 24;
+console.log(myage);
+
+
+
+
+export  {}; //temporary solution for removing the error.
