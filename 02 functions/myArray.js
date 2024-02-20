b398fa5b5d9fb6f1d8632cf7f2501dbc1e680bd5
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-20 21:54:33 +0530

    Arrays code

---
+++ 02 functions/myArray.js
@@ -0,0 +1,16 @@
+//Arrays in Ts
+// const hashrias = [];
+// hashrias.push("Giyu"); // this will give error as hashrias is type of never and we cannot assign string to it
+//below  is the solution >> how we define the array.
+var hashirays = []; //now hashrias is a type of string array.
+hashirays.push("Giyu");
+console.log(hashirays);
+//number array
+var hashiraPower = []; // now hashiraPower is type number array
+hashiraPower.push(2);
+console.log(hashiraPower);
+//another way of defining type of the arrays is
+var power = []; //now the power is a type of array of numbers.
+var allUser = []; // allUser is type of user array
+allUser.push({ name: "Sahil", isActive: true });
+console.log(allUser);
--- 02 functions/myArray.ts
+++ 02 functions/myArray.ts
@@ -1,1 +1,28 @@
 //Arrays in Ts
+
+// const hashrias = [];
+// hashrias.push("Giyu"); // this will give error as hashrias is type of never and we cannot assign string to it
+//below  is the solution >> how we define the array.
+
+const hashirays: string[] = []; //now hashrias is a type of string array.
+hashirays.push("Giyu");
+console.log(hashirays);
+
+//number array
+const hashiraPower: number[] = []; // now hashiraPower is type number array
+hashiraPower.push(2);
+console.log(hashiraPower);
+
+//another way of defining type of the arrays is
+const power: Array<number> = []; //now the power is a type of array of numbers.
+
+
+//another example with our User type which we will define
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUser: User[] = []; // allUser is type of user array
+allUser.push({name: "Sahil", isActive: true})
+console.log(allUser);
