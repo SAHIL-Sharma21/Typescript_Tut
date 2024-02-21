74eaaa0bbba1671abcf1a65c2f1ffeada11da635
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-21 22:13:13 +0530

    Union types

---
+++ 02 functions/myUnion.ts
@@ -0,0 +1,7 @@
+// HERE we are going to see about union types.
+// Union is a combination of 2 or 3 or more types of data that we can include in a variable, Array and etc...
+
+//union types eg:
+let score: number | string = 45; // now score can be number or string
+score = "4563"; //we are assigning string to score.
+console.log(score);
