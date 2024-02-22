8162579b111161ec615c3ad60af48631d4169447
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-22 21:56:46 +0530

    tuple intro

--- 02 basics/myTupEnum.ts
+++ 02 basics/myTupEnum.ts
@@ -4,6 +4,15 @@ const user: (string | number)[] = ["Sahil", 3]; //user variables with both strin
 
 // there are special cases or situation where we want to specific things to be pushed down to our array. >> usually like in API Call.
 // concept of tuple
+// defining tuple
+let tuser: [string, number, boolean];
+tuser = ["Sahil", 25, true]; // we can do this
+// tuser = [true, "Sahil", 25]; // but we cannot do this as it is tuple.
+console.log(tuser);
+
+
+// real example 
+
 
 
 
