ff44d1e5dcc350604b2a9a08d4628564abea4efd
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-25 21:04:44 +0530

    classes in Ts

--- 03 Pure Ts/dist/index.js
+++ 03 Pure Ts/dist/index.js
@@ -2,13 +2,26 @@
 // here we willl write our TypeScript code.
 // here we will write our production ts code.
 // now writing our ts file and checking
-console.log("TypeScript is here!!");
+// console.log("TypeScript is here!!");
 //tsc -w  command to run the ts file and in watch mode an keep track of it whenever there is change in our ts file.
-console.log("Typescript is again good!!");
+// console.log("Typescript is again good!!");
 let userName;
 userName = "Sahil sharma";
-console.log(userName);
+// console.log(userName);
 let userList = [24, 67, 78, 12, 55];
 userList.forEach((num) => console.log(num));
 const newList = userList.map((num) => (num * 3));
-console.log(newList);
+//  console.log(newList);
+//classes in Ts
+//this is how we define the classes in Ts
+class User {
+    constructor(email, name) {
+        this.city = "delhi"; //just giving initiator so that it will not give error, we are not using it in our constructor function
+        this.email = email;
+        this.name = name;
+    }
+}
+// creating object from our User class
+const sahil = new User("sahiil@gmaail.com", "Sahil");
+//  sahil.city = "delhi";
+console.log(sahil.city);
--- 03 Pure Ts/src/index.ts
+++ 03 Pure Ts/src/index.ts
@@ -2,18 +2,37 @@
 // here we will write our production ts code.
 
 // now writing our ts file and checking
-console.log("TypeScript is here!!");
+// console.log("TypeScript is here!!");
 //tsc -w  command to run the ts file and in watch mode an keep track of it whenever there is change in our ts file.
 
-console.log("Typescript is again good!!");
+// console.log("Typescript is again good!!");
 
 let userName: string;
 userName = "Sahil sharma";
-console.log(userName);
+// console.log(userName);
 
 let userList: number[] = [24, 67, 78, 12, 55];
  userList.forEach((num) => console.log(num));
 
  const newList: number[] = userList.map((num) => (num * 3));
- console.log(newList);
+//  console.log(newList);
+ 
+ //classes in Ts
+ //this is how we define the classes in Ts
+ class User {
+    //make sure that Ts ia aware of email and name in advance this is the right syntaxx..
+    email: string
+    name: string
+    readonly city: string = "delhi" //just giving initiator so that it will not give error, we are not using it in our constructor function
+
+    constructor(email: string, name: string){
+        this.email = email;
+        this.name = name;
+    }
+ }
+
+ // creating object from our User class
+ const sahil = new User("sahiil@gmaail.com", "Sahil");
+//  sahil.city = "delhi";
+ console.log(sahil.city);
  
