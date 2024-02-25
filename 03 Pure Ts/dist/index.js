5bc64aaf5ccbacd2a88f47d8f2966a7f1d22f66a
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-25 21:27:06 +0530

    Public & Private in classes

--- 03 Pure Ts/dist/index.js
+++ 03 Pure Ts/dist/index.js
@@ -17,11 +17,26 @@ const newList = userList.map((num) => (num * 3));
 class User {
     constructor(email, name) {
         this.city = "delhi"; //just giving initiator so that it will not give error, we are not using it in our constructor function
+        //making private one of the property: we can only access private property in class only but cannot access it outside
+        this.income = "3.0cr"; //there is no such thing in js world it has #income as aan examlem in js.
         this.email = email;
         this.name = name;
+        this.income; // here we are accessing private property
+        this.status = true;
     }
 }
 // creating object from our User class
 const sahil = new User("sahiil@gmaail.com", "Sahil");
 //  sahil.city = "delhi";
 console.log(sahil.city);
+console.log(sahil.name);
+console.log(sahil.status);
+// how classes are writen by profesinal devloper
+class Ticket {
+    constructor(ticketNumber, ticketIssueDate, ticketOwner) {
+        this.ticketNumber = ticketNumber;
+        this.ticketIssueDate = ticketIssueDate;
+        this.ticketOwner = ticketOwner;
+        this.ticketId = "XYWI_HHWNNSAQWUI_NMSJ12327829";
+    }
+}
--- 03 Pure Ts/src/index.ts
+++ 03 Pure Ts/src/index.ts
@@ -24,10 +24,16 @@ let userList: number[] = [24, 67, 78, 12, 55];
     email: string
     name: string
     readonly city: string = "delhi" //just giving initiator so that it will not give error, we are not using it in our constructor function
+    //making private one of the property: we can only access private property in class only but cannot access it outside
+    private income:  string= "3.0cr" //there is no such thing in js world it has #income as aan examlem in js.
 
+    //making public property in class
+    public status: boolean;
     constructor(email: string, name: string){
         this.email = email;
         this.name = name;
+        this.income; // here we are accessing private property
+        this.status = true;
     }
  }
 
@@ -35,4 +41,18 @@ let userList: number[] = [24, 67, 78, 12, 55];
  const sahil = new User("sahiil@gmaail.com", "Sahil");
 //  sahil.city = "delhi";
  console.log(sahil.city);
- 
+ console.log(sahil.name);
+ console.log(sahil.status);
+  
+
+ // how classes are writen by profesinal devloper
+ class Ticket {
+
+    readonly ticketId: string = "XYWI_HHWNNSAQWUI_NMSJ12327829"
+    constructor(
+        public ticketNumber: number, 
+        public ticketIssueDate: string,
+        private ticketOwner: string
+        ){
+    }
+ }
